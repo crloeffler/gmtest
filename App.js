19c4a0639799b0626128b241ae92102699c78485
crloeffler <crloeffler@gmail.com>
2021-10-03 12:34:25 -0700

    done

--- App.js
+++ App.js
@@ -6,8 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,97 +15,87 @@ import {
   Text,
   useColorScheme,
   View,
+  ActivityIndicator,
+  TouchableOpacity
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
-const App: () => Node = () => {
+export const SongRow = ({ artistName, trackName, collectionPrice, primaryGenreName }) => {
+  return (
+    <View style={styles.result}>
+      <View style={styles.resultRow}>
+        <Text>Artist </Text>
+        <Text testId="artistName">{artistName}</Text>
+      </View>
+
+      <View style={styles.resultRow}>
+        <Text>trackName </Text>
+        <Text testId="trackName">{trackName}</Text>
+      </View>
+      <View style={styles.resultRow}>
+        <Text>TrackPrice </Text>
+        <Text testId="collectionPrice">{collectionPrice}</Text>
+      </View>
+      <View style={styles.resultRow}>
+        <Text>primaryGenreName </Text>
+        <Text testId="primaryGenreName">{primaryGenreName}</Text>
+      </View>
+
+    </View>
+  )
+}
+
+const App = () => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
+  const getInfo = () => {
+    setData({});
+    setLoading(true)
+    fetch('https://itunes.apple.com/search?term=prince')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setData(data);
+        setLoading(false)
+      });
+  }
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        <TouchableOpacity onPress={getInfo}>
+          <Text testId="show_records">Show Records</Text>
+        </TouchableOpacity>
+
+        {loading && <ActivityIndicator size="large" />}
+        {data.results?.map((song) =>
+          <SongRow artistName={song.artistName} trackName={song.trackName} collectionPrice={song.collectionPrice} primaryGenreName={song.primaryGenreName} />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  result: {
+    margin: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  resultRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 });
 
 export default App;
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,42 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App, { SongRow } from '../App';
 
-// Note: test renderer must be required after react-native.
+
 import renderer from 'react-test-renderer';
+const tree = renderer.create(<App />)
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+const testIds = {
+  artistName: 'artistName',
+  trackName: 'trackName',
+  collectionPrice: 'collectionPrice',
+  primaryGenreName: 'primaryGenreName',
+}
+
+
+it('snapshot', () => {
+  expect(tree).toMatchSnapshot();
+});
+
+
+test('pass args to song row', () => {
+
+  const artistName = 'prince';
+  const trackName = 'purpleRain';
+  const collectionPrice = 1.00;
+  const primaryGenreName = 'r&b';
+
+  const testRenderer = renderer.create(<SongRow artistName={artistName} trackName={trackName} collectionPrice={collectionPrice} primaryGenreName={primaryGenreName} />);
+  const testInstance = testRenderer.root;
+
+  const artistField = testInstance.findByProps({ testId: testIds.artistName }).props
+  const trackField = testInstance.findByProps({ testId: testIds.trackName }).props
+  const priceField = testInstance.findByProps({ testId: testIds.collectionPrice }).props
+  const genereField = testInstance.findByProps({ testId: testIds.primaryGenreName }).props
+
+  expect(artistField.children).toEqual(artistName);
+  expect(trackField.children).toEqual(trackName);
+  expect(priceField.children).toEqual(collectionPrice);
+  expect(genereField.children).toEqual(primaryGenreName);
 });
